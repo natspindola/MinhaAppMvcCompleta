453c4ad54f3c344e276e12ed14154a43669f3e51
Perk <natspindola@hotmail.com>
2022-06-14 19:49:33 -0300

    mapeamento de classes

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context
+{
+    class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
