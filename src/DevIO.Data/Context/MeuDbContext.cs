dd65033bf60998dfacf4c46fc148188deac02f3f
Perk <natspindola@hotmail.com>
2022-06-15 17:02:50 -0300

    cascate delete

--- src/DevIO.Data/Context/MeuDbContext.cs
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using AppMvcBasica.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace DevIO.Data.Context
 {
@@ -13,5 +14,22 @@ namespace DevIO.Data.Context
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
         public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //se esquecer de mapear algo diretamente no mapping
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                .Where(p => p.ClrType == typeof(string))))
+                property.Relational().ColumnType = "varchar(100)";
+
+            //registrando os mappings na aplicação
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            //desabilitando o cascate delete
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
