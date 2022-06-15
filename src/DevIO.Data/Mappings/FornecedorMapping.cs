d3c3eb6e972715d482a52755c0c483e2e4aed5ae
Perk <natspindola@hotmail.com>
2022-06-15 16:51:52 -0300

    separando o mapping em classes

---
+++ src/DevIO.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,33 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(keyExpression: p => p.Id);
+
+            builder.Property(p => p.Nome)
+                   .IsRequired()
+                   .HasColumnType("varchar(200)");
+
+            builder.Property(p => p.Documento)
+                   .IsRequired()
+                   .HasColumnType("varchar(14)");
+
+            // 1: 1 => Fornecedor : Endereco
+            builder.HasOne(navigationExpression: f => f.Endereco)
+                .WithOne(navigationExpression: e => e.Fornecedor);
+
+            // 1: N => Fornecedor : Produtos
+            builder.HasMany(navigationExpression: f => f.Produtos)
+                .WithOne(navigationExpression: p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorId);
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}
