8ac10cfd473239e2b5cd53ecfa69d53222b0f2f2
Perk <natspindola@hotmail.com>
2022-06-17 17:01:34 -0300

    repositorio produtos

---
+++ src/DevIO.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,31 @@
+﻿using AppMvcBasica.Models;
+using DevIO.Business.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Repository
+{
+    class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public async Task<Produto> ObterProdutoFornecedor(Guid id)
+        {
+            return await Db.Produtos.AsNoTracking().Include(navigationPropertyPath: f => f.Fornecedor)
+                .FirstOrDefaultAsync(predicate: p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
+        {
+            return await Db.Produtos.AsNoTracking().Include(navigationPropertyPath: f => f.Fornecedor)
+                .OrderBy(prop => prop.Nome).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(predicate: p => p.FornecedorId == fornecedorId);
+        }
+    }
+}
