49e5acc36e5c712ac48f063172c81d7922914651
Perk <natspindola@hotmail.com>
2022-06-21 21:36:03 -0300

    Fornecedores Controller

--- DevIO.App/ViewModels/ProdutoViewModel.cs
+++ DevIO.App/ViewModels/ProdutoViewModel.cs
@@ -14,6 +14,10 @@ namespace DevIO.App.ViewModels
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("Fornecedor")]
+        public Guid FornedecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 2)]
         public string Nome { get; set; }
 
@@ -26,7 +30,7 @@ namespace DevIO.App.ViewModels
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 2)]
         public string Imagem { get; set; }
 
-        public IFormFile ImagemUpload { get; set; }
+        //public IFormFile ImagemUpload { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Valor { get; set; }
