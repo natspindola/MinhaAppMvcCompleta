6cab1a0e5a6b764a59ea929c813d4566b3500679
Perk <natspindola@hotmail.com>
2022-06-29 22:12:44 -0300

    function buscaCep

--- DevIO.App/wwwroot/js/site.js
+++ DevIO.App/wwwroot/js/site.js
@@ -38,3 +38,62 @@
         }
     });
 }
+
+function BuscaCep() {
+    $(document).ready(function () {
+        function limpa_formulario_cep() {
+            //limpa valores do formulário
+            $("#Endereco_Logradouro").val("");
+            $("#Endereco_Bairro").val("");
+            $("#Endereco_Cidade").val("");
+            $("#Endereco_Estado").val("");
+        }
+
+        //quando o campo cep perde o foco
+        $("#Endereco_Cep").blur(function () {
+
+            //nova variável cep somente com dígitos
+            var cep = $(this).val().replace(/\D/g, '');
+
+            //verifica se campo cep possui valor informado
+            if (cep != "") {
+
+                var validacep = /^[0-9]{8}$/;
+
+                //valida o formato do cep
+                if (validacep.test(cep)) {
+
+                    //preenche os campos com "..." enquanto consulta no webservice
+                    $("#Endereco_Logradouro").val("...");
+                    $("#Endereco_Bairro").val("...");
+                    $("#Endereco_Cidade").val("...");
+                    $("#Endereco_Estado").val("...");
+
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+                        if (!("erro" in dados)) {
+                            //atualiza os campos com os valores da consulta
+                            $("#Endereco_Logradouro").val(dados.logradouro);
+                            $("#Endereco_Bairro").val(dados.bairro);
+                            $("#Endereco_Cidade").val(dados.localidade);
+                            $("#Endereco_Estado").val(dados.uf);
+                        }
+                        else {
+                            //cep pesquisado não encontrado
+                            limpa_formulario_cep();
+                            alert("CEP não encontrado.")
+                        }
+                    });
+                }
+                else {
+                    //cep inválido
+                    limpa_formulario_cep();
+                    alert("Formato de CEP inválido.")
+                }
+            }
+            else {
+                    //cep sem valor
+                    limpa_formulario_cep();
+            };
+        });
+    });
+}
