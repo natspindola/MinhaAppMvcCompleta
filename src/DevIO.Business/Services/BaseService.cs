9dd875227e1f2cb7c87c1e49b0786f1f272047e7
Perk <natspindola@hotmail.com>
2022-07-01 18:40:11 -0300

    Organizando classes service

--- src/DevIO.Business/Services/BaseService.cs
+++ src/DevIO.Business/Services/BaseService.cs
@@ -1,19 +1,6 @@
-﻿
-using DevIO.Business.Interfaces;
-
-namespace DevIO.Business.Services
+﻿namespace DevIO.Business.Services
 {
     public abstract class BaseService
-    {
-
-    }
-
-    public class FornecedorService : BaseService, IFornecedorService
-    {
-
-    }
-
-    public class ProdutoService : BaseService, IProdutoService
     {
 
     }
---
+++ src/DevIO.Business/Services/FornecedorService.cs
@@ -0,0 +1,31 @@
+﻿
+using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        public Task Adicionar(Fornecedor fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Atualizar(Fornecedor fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarEndereo(Endereco endereco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
