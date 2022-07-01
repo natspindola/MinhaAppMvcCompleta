5f63675253313aba870c073db5c233b060cc6897
Perk <natspindola@hotmail.com>
2022-07-01 19:34:53 -0300

    CPF validação

---
+++ src/DevIO.Business/Models/Validations/Documentos/ValidacaoDocs.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+
+namespace DevIO.Business.Models.Validations.Documentos
+{
+    public class CpfValidacao
+    {
+        public const int TamanhoCpf = 11;
+
+        public static bool Validar(string cpf)
+        {
+            var cpfNumeros = Utils.ApenasNumeros(cpf);
+
+            if (!TamanhoValido(cpfNumeros)) return false;
+            return !TemDigitosRepetidos(cpfNumeros) && TemDigitosValidos(cpfNumeros);
+        }
+
+        private static bool TamanhoValido(string valor)
+        {
+            return valor.Length == TamanhoCpf;
+        }
+
+        private static bool TemDigitosRepetidos(string valor)
+        {
+            string[] invalidNumbers =
+            {
+                "00000000000",
+                "11111111111",
+                "22222222222",
+                "33333333333",
+                "44444444444",
+                "55555555555",
+                "66666666666",
+                "77777777777",
+                "88888888888",
+                "99999999999"
+            };
+            return invalidNumbers.Contains(valor);
+        }
+
+        private static bool TemDigitosValidos(string valor)
+        {
+            var number = valor.Substring(startIndex: 0, length: TamanhoCpf - 2);
+            var digitoVerificador = new DigitoVerificador(number)
+                .ComMultiplicadoresAte(primeiroMultiplicador: 2, ultimoMultiplicador: 11)
+                .Substituindo(substituto: "0", paramsdigitos: 10, 11);
+            var firstDigit = digitoVerificador.CalculaDigito();
+            digitoVerificador.AddDigito(firstDigit);
+            var secondDigit = digitoVerificador.CalculaDigito();
+
+            return string.Concat(firstDigit, secondDigit) == valor.Substring(startIndex: TamanhoCpf - 2, length: 2);
+        }      
+
+    }
+}
