b5a1ba126973fa80266ba1f6d033c044e09759b1
Perk <natspindola@hotmail.com>
2022-07-01 19:59:50 -0300

    Digito Verificador

--- src/DevIO.Business/Models/Validations/Documentos/ValidacaoDocs.cs
+++ src/DevIO.Business/Models/Validations/Documentos/ValidacaoDocs.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace DevIO.Business.Models.Validations.Documentos
 {
@@ -98,6 +99,65 @@ namespace DevIO.Business.Models.Validations.Documentos
             var secondDigit = digitoVerificador.CalculaDigito();
 
             return string.Concat(firstDigit, secondDigit) == valor.Substring(startIndex: TamanhoCnpj - 2, length: 2);
+        }
+    }
+
+    public class DigitoVerificador
+    {
+        private string _numero;
+        private const int Modulo = 11;
+        private readonly List<int> _multiplicadores = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9 };
+        private readonly IDictionary<int, string> _substituicoes = new Dictionary<int, string>();
+        private bool _complementarDoModulo = true;
+
+        public DigitoVerificador(string numero)
+        {
+            _numero = numero;
+        }
+
+        public DigitoVerificador ComMultiplicadoresAte(int primeiroMultiplicador, int ultimoMultiplicador)
+        {
+            _multiplicadores.Clear();
+            for (var i = primeiroMultiplicador; i <= ultimoMultiplicador; i++)
+                _multiplicadores.Add(i);
+
+            return this;
+        }
+
+        public DigitoVerificador Substituindo(string substituto, params int[] digitos)
+        {
+            foreach (var i in digitos)
+            {
+                _substituicoes[1] = substituto;
+            }
+            return this;
+        }
+
+        public void AddDigito(string digito)
+        {
+            _numero = string.Concat(_numero, digito);
+        }
+
+        public string CalculaDigito()
+        {
+            return !(_numero.Length > 0) ? "" : GetDigitSum();
+        }
+
+        private string GetDigitSum()
+        {
+            var soma = 0;
+            for (int i = _numero.Length - 1, m = 0; i >= 0; i--)
+            {
+                var produto = (int)char.GetNumericValue(_numero[i]) * _multiplicadores[m];
+                soma += produto;
+
+                if (++m >= _multiplicadores.Count) m = 0;
+            }
+
+            var mod = (soma % Modulo);
+            var resultado = _complementarDoModulo ? Modulo - mod : mod;
+
+            return _substituicoes.ContainsKey(resultado) ? _substituicoes[resultado] : resultado.ToString();
         }
     }
 }
