16aa17dc06edbf496ebce3bbbc7a8746d8a2882e
Perk <natspindola@hotmail.com>
2022-07-06 17:50:38 -0300

    Classe SumaryView

---
+++ DevIO.App/Extensions/SumaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DevIO.App.Extensions
+{
+    public class SumaryViewComponent : ViewComponent
+    {
+        private readonly INotificador _notificador;
+
+        public SumaryViewComponent(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var notificacoes = await Task.FromResult(_notificador.ObterNotificacoes());
+
+            notificacoes.ForEach(c=> ViewData.ModelState.AddModelError(key:string.Empty, errorMessage:c.Mensagem));
+
+            return View();
+        }
+    }
+}
