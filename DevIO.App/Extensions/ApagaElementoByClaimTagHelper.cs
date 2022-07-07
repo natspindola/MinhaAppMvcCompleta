7a63e0bfd97563996521d52c897d070cf71cdee8
Perk <natspindola@hotmail.com>
2022-07-07 18:06:55 -0300

    Desabilitar link com Claim

--- DevIO.App/Extensions/ApagaElementoByClaimTagHelper.cs
+++ DevIO.App/Extensions/ApagaElementoByClaimTagHelper.cs
@@ -16,11 +16,9 @@ namespace DevIO.App.Extensions
         }
 
         [HtmlAttributeName("supress-by-claim-name")]
-
         public string IdentityClaimName { get; set; }
 
         [HtmlAttributeName("supress-by-claim-value")]
-
         public string IdentityClaimValue { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
@@ -35,6 +33,40 @@ namespace DevIO.App.Extensions
             if (temAcesso) return;
 
             output.SuppressOutput();
+        }
+    }
+
+    [HtmlTargetElement("a", Attributes = "disable-by-claim-name")]
+    [HtmlTargetElement("a", Attributes = "disable-by-claim-value")]
+    public class DesabilitaLinkByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public DesabilitaLinkByClaimTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("disable-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("disable-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(paramName: nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(paramName: nameof(output));
+
+            var temAcesso = CustomAuthorization.ValidarClaimsUsuario(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.Attributes.RemoveAll(name: "href");
+            output.Attributes.Add(new TagHelperAttribute(name: "style", value: "cursor: not-allowed"));
+            output.Attributes.Add(new TagHelperAttribute(name: "title", value: "Você não tem permissão."));
         }
     }
 }
