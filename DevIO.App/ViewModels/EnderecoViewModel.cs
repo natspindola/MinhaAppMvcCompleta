c34e5f591d96f6a5ebc821676f56d1aeb5724f86
Perk <natspindola@hotmail.com>
2022-07-08 21:11:48 -0300

    Atualizações

--- DevIO.App/ViewModels/EnderecoViewModel.cs
+++ DevIO.App/ViewModels/EnderecoViewModel.cs
@@ -17,10 +17,9 @@ namespace DevIO.App.ViewModels
         public string Logradouro { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 2)]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 0)]
         public string Numero { get; set; }
 
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 2)]
         public string Complemento { get; set; }
 
