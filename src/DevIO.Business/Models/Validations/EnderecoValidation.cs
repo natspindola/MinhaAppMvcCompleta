f13b5162faa3c3d6d645f15c9932c21707319b87
Perk <natspindola@hotmail.com>
2022-07-08 21:37:23 -0300

    Correção - Endereço

--- src/DevIO.Business/Models/Validations/EnderecoValidation.cs
+++ src/DevIO.Business/Models/Validations/EnderecoValidation.cs
@@ -10,31 +10,30 @@ namespace DevIO.Business.Models.Validations
         {
             RuleFor(expression: c => c.Logradouro)
                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
-                .Length(min: 2, max: 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caractéres.");
+                .Length(min: 2, max: 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
 
             RuleFor(expression: c => c.Bairro)
                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
-                .Length(min: 2, max: 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caractéres.");
+                .Length(min: 2, max: 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
 
             RuleFor(expression: c => c.Cep)
                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
-                .Length(8).WithMessage("O campo {PropertyName} precisa ter {MaxLength} caractéres.");
+                .Length(8).WithMessage("O campo {PropertyName} precisa ter {MaxLength} caracteres.");
 
             RuleFor(expression: c => c.Cidade)
                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
-                .Length(min: 2, max: 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caractéres.");
+                .Length(min: 2, max: 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
 
             RuleFor(expression: c => c.Estado)
                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
-                .Length(min: 2, max: 20).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caractéres.");
+                .Length(min: 2, max: 20).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
 
             RuleFor(expression: c => c.Logradouro)
                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
-                .Length(min: 2, max: 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caractéres.");
+                .Length(min: 2, max: 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
 
             RuleFor(expression: c => c.Numero)
-                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
-                .Length(min: 2, max: 20).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caractéres.");
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido");
         }
     }
 }
